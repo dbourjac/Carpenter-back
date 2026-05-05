72de0eb1b50c1f92e11c221954388ce3f1a01a65
Dylan Bourjac <110423970+dbourjac@users.noreply.github.com>
2026-05-05 16:39:26 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ app.set('trust proxy', 1);
 
 // ── Middlewares globales ─────────────────────────────────────────
 app.use(cors({
-  origin: process.env.FRONTEND_URL ? process.env.FRONTEND_URL : 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://carpenter-front.onrender.com'
+  ] ? process.env.FRONTEND_URL : 'http://localhost:5173',
   credentials: true,
 }));
 
